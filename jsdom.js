8dff1febe2f17886ebcaa82766179afce2485cee
spidunno <70819261+spidunno@users.noreply.github.com>
2021-08-12 20:07:36 +0000

    Commented EVERY. SINGLE. FUNCTION. I. WANT. TO. DIE

--- jsdom.js
+++ jsdom.js
@@ -1,181 +1,221 @@
 class Component {
-	constructor(tagName, children = [""], attributes = {}) {
-		this.tagName = tagName;
-		this.children = (children instanceof Array ? children : [children]);
-		this.attributes = {};
-		this.elem = document.createElement(this.tagName);
+  constructor(tagName, children = [""], attributes = {}) {
+    this.tagName = tagName;
+    this.children = (children instanceof Array ? children : [children]);
+    this.attributes = {};
+    this.elem = document.createElement(this.tagName);
     if (attributes !== null) {
-		for(var i in this.elem.attributes) {
-			this.attributes[i] = this.elem.attributes[i];
-			for(var j in attributes) {
-				this.attributes[j] = attributes[j];
-			}
-		}
-		for(var i in this.attributes) {
-			this.elem.setAttribute(i, this.attributes[i]);
-		};
-	}
+      for (var i in this.elem.attributes) {
+        this.attributes[i] = this.elem.attributes[i];
+        for (var j in attributes) {
+          this.attributes[j] = attributes[j];
+        }
+      }
+      for (var i in this.attributes) {
+        this.elem.setAttribute(i, this.attributes[i]);
+      };
+    }
   }
 }
 
-// function toElem(component) {
-//   let elem = document.createElement(component.tagName);
-//   for (var i in component.children) {
-//     if (typeof component.children[i] === "string") {
-//       elem.append(component.children[i]);
-//     }
-//     else if (component.children[i] instanceof Component) {
-//       elem.append(toElem(component.children[i]));
-//     }
-//   }
-//   return elem
-// }
-
 class JsDom {
-	static extend(func, props) {
-		for(var prop in props) {
-			if(props.hasOwnProperty(prop)) {
-				func[prop] = props[prop];
-			}
-		}
-		return func;
-	}
-}
-
-var j = JsDom.extend(function(tagName, children = [""], attributes = {}) {
-	return new Component(tagName, children, attributes);
-}, {
-  jsx: function(tagName, attributes = {}, children = [""],) {
-    
-	return new Component(...[tagName, children, attributes]);
-},
-	toElem: function(component) {
-    let elem;
-    if (component instanceof Component) {
-      elem = document.createElement(component.tagName);
-		  for(var i in component.attributes) {
-		  	elem.setAttribute(i, component.attributes[i])
-		  }
-		  for(var i in component.children) {
-		  	if(typeof component.children[i] === "string") {
-		  		elem.append(component.children[i]);
-		  	} 
-        else if (component.children[i] instanceof Array) {
-          elem.append(j.toElem(new Component("div", component.children[i])));
+  /* Stop rendering interval */
+  static haltRender() {
+    clearInterval(this.interval);
+  }
+  /* Start rendering interval */
+  static render(component, renderElem = document.body, parseSymbs = ["{{", "}}"]) {
+    /* Set variable to interval to stop later */
+    this.interval = setInterval(function () {
+      /* Convert component into HTML element */
+      let elem = JsDom.toElem(component);
+      /* loop through component's children */
+      for (var i in component.children) {
+        /* Only update element if contents are not equal */
+        if (JsDom.evalReplace(JsDom.parse(elem.innerHTML, parseSymbs)) !== JsDom.evalReplace(JsDom.parse(renderElem.innerHTML, parseSymbs))) {
+          /* If so, update HTML */
+          renderElem.innerHTML = JsDom.evalReplace(JsDom.parse(elem.innerHTML, parseSymbs));
         }
-        else if(component.children[i] instanceof Component) {
-		  		elem.append(j.toElem(component.children[i]));
-		  	}
-		  }
+      }
+    }, 2.5);
+  }
+  /* Convert a string containing HTML into a component/components */
+  static html(html) {
+    /* Use built in DOMParser API to parse string into HTML Element */
+    let parsed = new DOMParser().parseFromString(html, 'text/html').body.childNodes;
+    /* Initialize array to add components to */
+    let comp = [];
+    /* Loop through all child nodes of parsed HTML */
+    for (var i in parsed) {
+      /* Create and push a new component with all of the element's data into comp */
+      comp.push(new Component(parsed[i].localName, parsed[i], parsed[i][0]));
     }
+    /* return a div component containing all of the other components */
+    return new Component("div", comp);
+  }
+  /* UUID function for use in parsing text; Taken from https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid */
+  static uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  /* Evaluate and replace text returned from parse() function */
+  static evalReplace(obj) {
+    /* get the str from obj */
+    let newStr = obj["str"];
+    /* Loop through all expressions used in the text */
+    for (var i in obj["exprs"]) {
+      /* replace all of the uuids with the output their respective expressions */
+      newStr = newStr.replace(obj["map"][i], eval(obj["exprs"][i]));
+    }
+    /* return the string */
+    return newStr;
+  }
+  /* createElement Function */
+  static createElement(tagName, children = [""], attributes = {}) {
+    return new Component(tagName, children, attributes);
+  }
+  /* convert objects of class Component into HTML Elements */
+  static toElem(component) {
+    /* instantiate elem variable to set later */
+    let elem;
+    /* check if component is an instance of the Component class */
+    if (component instanceof Component) {
+      /* if so set elem to an HTML element with a tagName of the component tagName */
+      elem = document.createElement(component.tagName);
+      /* loop through all of the components attributes and set the HTML element's attributes to that */
+      for (var i in component.attributes) {
+        elem.setAttribute(i, component.attributes[i])
+      }
+      /* loop through all of the children of the component */
+      for (var i in component.children) {
+        /* if child is a string, append it to the element */
+        if (typeof component.children[i] === "string") {
+          elem.append(component.children[i]);
+        }
+        /* if the child is an array, run this function again with a component with its children set to the array, and append that to the element */
+        else if (component.children[i] instanceof Array) {
+          elem.append(JsDom.toElem(new Component("div", component.children[i])));
+        }
+        /* if the child is another component, run this function again with said component, and append that to the element */
+        else if (component.children[i] instanceof Component) {
+          elem.append(JsDom.toElem(component.children[i]));
+        }
+      }
+    }
+    /* if the component is an array run this function again with a component whos children is said array, and append that to the element */
     else if (component instanceof Array) {
-      elem = j.toElem(new Component("div", component));
+      elem = JsDom.toElem(new Component("div", component));
     }
+    /* if the component is a string, append a new text node to the element */
     else if (typeof component === "string") {
       for (var i in component) {
         elem = document.createTextNode(component[i]);
       }
     }
-		return elem
-	},
-	parse: function(str, identifiers = ["{{", "}}"]) {
-		/* Define misc variables */
-		var currentUUID = "";
-		let exprIndex = 0;
-		let parsedStr = [];
-		let keys = [];
-		let strNoExpr = "";
-		let indexes = [];
-		let inExpr = false;
-		let skipNextChar = 0;
-		let nextArr = [];
-
-		/* Main loop */
-		for(var i = 0; i < str.length; i++) {
-			if(inExpr === false) {
-				if(skipNextChar > 0) {
-					skipNextChar--;
-					continue
-				}
-				/* Check if string matches characters */
-				if(str.substring(i, i + (identifiers[0].length)) === identifiers[0]) {
-					nextArr = [];
-					/* continue loop if skipping next character in string */
-					/* Define the UUID for this loop and add that to the keys array for use after the function returns */
-					currentUUID = j.uuidv4();
-					keys[exprIndex] = currentUUID;
-					/* Push loop value to array for use in formatting string later */
-					nextArr.push(i);
-					/* Specify that loop is currently inside an expression and to skip next character */
-					inExpr = true;
-					skipNextChar = identifiers[0].length - 1;
-				}
-			} else if(inExpr === true) {
-				if(skipNextChar > 0) {
-					skipNextChar--;
-					continue
-				}
-				if(str.substring(i, i + identifiers[1].length) === identifiers[1]) {
-					nextArr.push(i + 2);
-					indexes.push(nextArr);
-					inExpr = false;
-					skipNextChar = identifiers[1].length - 1;
-					exprIndex++;
-				} else if(inExpr === true && skipNextChar === 0) {
-					parsedStr[exprIndex] = [parsedStr[exprIndex], str[i]].join("");
-				}
-			}
-		}
-		strNoExpr = str;
-		let lastKeyLength = 0;
-		for(var i in indexes) {
-			// console.log(lastKeyLength)
-			// console.log((parseInt(indexes[i][0]) + parseInt(lastKeyLength)) + " " + (parseInt(indexes[i][1]) + parseInt(lastKeyLength)))
-			// console.log(indexes[i][0], indexes[i][1])
-			strNoExpr = strNoExpr.substring(0, (indexes[i][0]) + (lastKeyLength)) + keys[i] + strNoExpr.substring((indexes[i][1]) + (lastKeyLength));
-			lastKeyLength += keys[i].length - (indexes[i][1] - indexes[i][0]);
-			// console.log(indexes[i][1] - indexes[i][0])
-		}
-		return {
-			"str": strNoExpr,
-			"map": keys,
-			"exprs": parsedStr
-		}
-	},
-	evalReplace: function(obj) {
-		let newStr = obj["str"];
-		for(var i in obj["exprs"]) {
-			newStr = newStr.replace(obj["map"][i], eval(obj["exprs"][i]));
-		}
-		return newStr;
-	},
-	uuidv4: function() {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			var r = Math.random() * 16 | 0,
-				v = c == 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
-	},
-	html: function(html) {
-    let parsed = new DOMParser().parseFromString(html, 'text/html').body.childNodes;
-    let comp = [];
-    for (var i in parsed) {
-      comp.push(new Component(parsed[i].localName, parsed[i], parsed[i][0]));
-    }
-    return new Component("div", comp);
-  },
-	render: function(component, renderElem = document.body, parseSymbs = ["{{", "}}"]) {
-		let interval = setInterval(function() {
-			let elem = j.toElem(component);
-			for(var i in component.children) {
-				if(j.evalReplace(j.parse(elem.innerHTML, identifiers = parseSymbs)) !== j.evalReplace(j.parse(renderElem.innerHTML, identifiers = parseSymbs))) {
-					renderElem.innerHTML = j.evalReplace(j.parse(elem.innerHTML, identifiers = parseSymbs));
-				}
-			}
-		}, 2.5);
-    this.interval = interval;
-	},
-  haltRender: function() {
-    clearInterval(this.interval);
+    /* return the element */
+    return elem
   }
+
+  /* function to parse out expressions from a string */
+  static parse(str, identifiers = ["{{", "}}"]) {
+    /* Define misc variables */
+    var currentUUID = "";
+    let exprIndex = 0;
+    let parsedStr = [];
+    let keys = [];
+    let strNoExpr = "";
+    let indexes = [];
+    let inExpr = false;
+    let skipNextChar = 0;
+    let nextArr = [];
+
+    /* Main loop */
+    for (var i = 0; i < str.length; i++) {
+      /* if not in an expression already check if in an expression */
+      if (inExpr === false) {
+        /* if skipNextChar is > 0, subtract one from it and skip this loop */
+        if (skipNextChar > 0) {
+          skipNextChar--;
+          continue
+        }
+        /* Check if string matches characters */
+        if (str.substring(i, i + (identifiers[0].length)) === identifiers[0]) {
+          nextArr = [];
+          /* continue loop if skipping next character in string */
+          /* Define the UUID for this loop and add that to the keys array for use after the function returns */
+          currentUUID = this.uuidv4();
+          keys[exprIndex] = currentUUID;
+          /* Push loop value to array for use in formatting string later */
+          nextArr.push(i);
+          /* Specify that loop is currently inside an expression and to skip next characters */
+          inExpr = true;
+          skipNextChar = identifiers[0].length - 1;
+        }
+      }
+      /* if already in an expression */
+      else if (inExpr === true) {
+        /* check if skipNextChar is > 0, if so subtract one and skip this loop */
+        if (skipNextChar > 0) {
+          skipNextChar--;
+          continue
+        }
+        /* perform the same check as before except for the closing characters */
+        if (str.substring(i, i + identifiers[1].length) === identifiers[1]) {
+          /* push the position in the string + 2 to the array to use to format string later */
+          nextArr.push(i + 2);
+          /* push all of nextArr to indexes to format string with */
+          indexes.push(nextArr);
+          /* leave current expression */
+          inExpr = false;
+          /* skip next few characters */
+          skipNextChar = identifiers[1].length - 1;
+          /* increase the index of the expression we're on by one */
+          exprIndex++;
+        }
+        /* if in expression and skipNextChar is 0 */
+        else if (inExpr === true && skipNextChar === 0) {
+          /* what the fuck */
+          parsedStr[exprIndex] = [parsedStr[exprIndex], str[i]].join("");
+        }
+      }
+    }
+    /* set the string with expressions removed to the input string */
+    strNoExpr = str;
+    /* set lastKeyLength to 0 */
+    let lastKeyLength = 0;
+    /* loop through all indexes of expressions */
+    for (var i in indexes) {
+      /* strip out all expressions and replace with the respective uuid */
+      strNoExpr = strNoExpr.substring(0, (indexes[i][0]) + (lastKeyLength)) + keys[i] + strNoExpr.substring((indexes[i][1]) + (lastKeyLength));
+      /* set lastKeyLength to the last used uuid to shift the indexes by */
+      lastKeyLength += keys[i].length - (indexes[i][1] - indexes[i][0]);
+    }
+    /* return all needed data */
+    return {
+      "str": strNoExpr,
+      "map": keys,
+      "exprs": parsedStr
+    }
+  }
+  /* extend function for allowing "j" to be both a function and an object, taken from https://stackoverflow.com/questions/4906675/javascript-function-have-sub-functions-variables */
+  static extend(func, props) {
+    for (var prop in props) {
+      if (props.hasOwnProperty(prop)) {
+        func[prop] = props[prop];
+      }
+    }
+    return func;
+  }
+}
+
+/* define "j" using JsDom.extend() */
+var j = JsDom.extend(JsDom.createElement, {
+  render: JsDom.render,
+  haltRender: JsDom.haltRender,
+  html: JsDom.html,
+
 });
